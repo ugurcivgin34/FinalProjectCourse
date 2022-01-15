4da066bc3a73546d392754e1556a23b620b3dc74
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-01-16 01:23:25 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        //Metotlar default da public dir.
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(int productId);
+        List<Product> GetAllByCategory(int categoryId);
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,74 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+                new Product{ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
+                new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitsInStock=2},
+                new Product{ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitsInStock=65},
+                new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice=85,UnitsInStock=1}
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(int productId)
+        {
+
+            Product productToDelete = null; //new Product(); şeklinde yazmaya gerek yok, zaten aşağıda linq sorgusu sonucu bulduğu değeri adresini atayacak,boşuna bellekte yer açmış olacaktı
+            //Ling kullanmasaydık
+            //foreach (var p in _products)
+            //{
+            //    if (product.ProductId == p.ProductId)
+            //    {
+            //        productToDelete = p;
+            //    }
+            //}
+
+            //LINQ - Language Integrated Query
+            //Id bazlı ifadelerde SingleOrDefault yazmak mantıklı olur
+            //productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId); //Bir eleman bulmaya yarar,yani tek tek dolaşır(SingleOrDefault)
+            productToDelete = _products.SingleOrDefault(p => p.ProductId == productId);
+
+
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+    
+        public void Update(Product product)
+        {
+           Product productToUpdate  = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            //Where koşulu içindeki şarta uyan bütün elamanları yeni bir liste haline getirir ve onu döndürür.Sql mantığı gibi
+           return  _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+    }
+}
