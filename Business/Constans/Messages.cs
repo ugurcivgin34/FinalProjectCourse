87559bd947ce1728bc275b5db1b9420660fa111d
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-01-23 22:55:12 +0300

    10.Ders Tamamlandı

---
+++ Business/Constans/Messages.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constans
+{
+    public  static class Messages
+    {
+        //private şeklinde yazsaydık değişken isimleri küçük harfle başlardı fakar public ile tanımladığımız için büyük harfle kullanılır
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        internal static string MaintenanceTime = "Sistem bakımda";
+        internal static string ProductsListed = "Ürünler listelendi";
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool success, string message) : this(success) //bu class da tek parametreli olanı da çalıştır demek . Yani aşağıdaki constructoru da çalıştırmış olur aynı zamanda
+        {
+            Message = message;
+        }
+        public Result(bool success)
+        {  
+            Success = success;
+        }
+        public bool Success { get; }
+        public string Message { get; }
+    }
+}
