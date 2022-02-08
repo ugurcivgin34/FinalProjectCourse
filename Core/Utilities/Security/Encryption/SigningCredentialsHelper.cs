664dac73ab92bfa52ff4f60a14b9b1d94e3468d6
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-02-08 11:55:15 +0300

    .

--- Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -10,7 +10,13 @@ namespace Core.Utilities.Security.Encryption
     public class SigningCredentialsHelper
     {
         // jwt servicelerinin web apide,web apinin kullanabileceği jwt larının oluşturabilmesi için
-        //bu şifrelemenin web apinin kendisininde ihtiyacı var,gelen jwt nin doğrulanması gerekiyor
+        //bu şifrelemenin web apinin kendisininde ihtiyacı var,gelen jwt nin doğrulanması gerekiyor.
+        //Bu yüzden aşağıdaki gibi hangi keyi kullancak hangi algoritmayı kullancak onları belirttik
+        //Zaten hashing helper de de belirtmiştik.O yüzden burda da belirtmemiz lzım 
+
+        //Kullanıcı bilgilerin aslında bizim cretentialslarımız oluyor
+
+        //Bir sisteme girmek için keye ihtiyaç var.O bizim credetialsımız yani
         public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey)
         {
             return new SigningCredentials(securityKey,SecurityAlgorithms.HmacSha512Signature);
